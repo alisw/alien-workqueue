609a86f6696df7cded67af860292e911acc7b0b4
Dario Berzano <dario.berzano@cern.ch>
2016-09-02 18:57:41 +0200

    Make job wrapper retain `ALIEN_*` variables
    Job wrapper runs jobs in a pristine environment, only `ALIEN_*` variables, set
    by the VOBOX, are retained.

--- alien_job_wrapper.sh
+++ alien_job_wrapper.sh
@@ -12,8 +12,12 @@ MINIENV=( "PATH=/bin:/usr/bin"
           "LC_NUMERIC=C"
           "LC_TIME=C"
           "LC_ALL=C" )
+while read -r -d $'\0' ENVVAR; do
+  # Preserve variables starting with ALIEN_ in the job environment
+  [[ "$ENVVAR" == ALIEN_* ]] && MINIENV+=("$ENVVAR") || true
+done < <(env -0)
 if [[ $ALIEN_USE_PARROT == 1 ]]; then
-  parrot_run -v > /dev/null 2>&1 || { echo "NOTE: Parrot not found, not using it"; exec env -i ${MINIENV[*]} "$@"; }
-  ls /cvmfs/alice.cern.ch 2>&1 > /dev/null || { echo "NOTE: using Parrot for CVMFS"; exec parrot_run env -i ${MINIENV[*]} "$@"; }
+  parrot_run -v > /dev/null 2>&1 || { echo "NOTE: Parrot not found, not using it"; exec env -i "${MINIENV[@]}" "$@"; }
+  ls /cvmfs/alice.cern.ch 2>&1 > /dev/null || { echo "NOTE: using Parrot for CVMFS"; exec parrot_run env -i "${MINIENV[@]}" "$@"; }
 fi
-exec env -i ${MINIENV[*]} "$@"
+exec env -i "${MINIENV[@]}" "$@"
